bdde38f7fb3de77ebe7fe79c1a65c9b15ec927a2
Mihai Popescu <mihai@cryptocube.ro>
2022-12-10 12:18:33 +0200

    lista scoli

--- resources/views/livewire/lista-component.blade.php
+++ resources/views/livewire/lista-component.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-gray-100  mx-4 lg:mx-0">
-    <div class="container bg-white py-8 lg:p-8 rounded-custom  ">
+    <div class="container bg-white py-8 lg:p-8 rounded-custom  lista-table">
         <livewire:schools-table class="mt-1"/>
     </div>
 </div>
