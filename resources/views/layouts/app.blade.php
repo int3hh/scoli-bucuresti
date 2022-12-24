168567a8c2e8523253d0cb46247565cb72ea7255
- <octav@cryptocube.ro>
2022-12-24 09:17:56 +0200

    change logo fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>:: ScoliBune.ro ::</title>
+        <title>ScoliBune.ro</title>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
        
